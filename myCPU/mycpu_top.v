cdf0e0935e1119f47e9fd0668a523358be433648
jennymarip <jennymarip830@gmail.com>
2024-11-28 15:10:56 +0800

    添加AXI转接桥接口

--- myCPU/mycpu_top.v
+++ myCPU/mycpu_top.v
@@ -51,38 +51,100 @@ module mycpu_top(
     output [31:0] debug_wb_rf_wdata
 );
 
-wire clk, resetn, inst_sram_req, inst_sram_wr, inst_sram_size, inst_sram_wstrb, inst_sram_addr, inst_sram_wdata, inst_sram_addr_ok, inst_sram_data_ok, inst_sram_rdata;
-wire data_sram_req, data_sram_wr, data_sram_size, data_sram_wstrb, data_sram_addr, data_sram_wdata, data_sram_addr_ok, data_sram_data_ok, data_sram_rdata;
-
 mycpu_sram cpu_core(
-    .clk    (clk),
-    .resetn (resetn),
+    .clk    (aclk   ),
+    .resetn (aresetn),
     // inst sram interface
-    .inst_sram_req     (inst_sram_req),
-    .inst_sram_wr      (inst_sram_wr),
-    .inst_sram_size    (inst_sram_size),
-    .inst_sram_wstrb   (inst_sram_wstrb),
-    .inst_sram_addr    (inst_sram_addr),
-    .inst_sram_wdata   (inst_sram_wdata),
+    .inst_sram_req     (inst_sram_req    ),
+    .inst_sram_wr      (inst_sram_wr     ),
+    .inst_sram_size    (inst_sram_size   ),
+    .inst_sram_wstrb   (inst_sram_wstrb  ),
+    .inst_sram_addr    (inst_sram_addr   ),
+    .inst_sram_wdata   (inst_sram_wdata  ),
     .inst_sram_addr_ok (inst_sram_addr_ok),
     .inst_sram_data_ok (inst_sram_data_ok),
-    .inst_sram_rdata   (inst_sram_rdata),
+    .inst_sram_rdata   (inst_sram_rdata  ),
     // data sram interface
-    .data_sram_req     (data_sram_req),
-    .data_sram_wr      (data_sram_wr),
-    .data_sram_size    (data_sram_size),
-    .data_sram_wstrb   (data_sram_wstrb),
-    .data_sram_addr    (data_sram_addr),
-    .data_sram_wdata   (data_sram_wdata),
+    .data_sram_req     (data_sram_req    ),
+    .data_sram_wr      (data_sram_wr     ),
+    .data_sram_size    (data_sram_size   ),
+    .data_sram_wstrb   (data_sram_wstrb  ),
+    .data_sram_addr    (data_sram_addr   ),
+    .data_sram_wdata   (data_sram_wdata  ),
     .data_sram_addr_ok (data_sram_addr_ok),
     .data_sram_data_ok (data_sram_data_ok),
-    .data_sram_rdata   (data_sram_rdata),
+    .data_sram_rdata   (data_sram_rdata  ),
     // trace debug interface
-    .debug_wb_pc       (debug_wb_pc),
-    .debug_wb_rf_wen   (debug_wb_rf_wen),
-    .debug_wb_rf_wnum  (debug_wb_rf_wnum),
+    .debug_wb_pc       (debug_wb_pc      ),
+    .debug_wb_rf_wen   (debug_wb_rf_wen  ),
+    .debug_wb_rf_wnum  (debug_wb_rf_wnum ),
     .debug_wb_rf_wdata (debug_wb_rf_wdata)
 );
 // 转接桥
-// ...
+sram2axi_bridge bridge(
+    .clk    (aclk   ),
+    .resetn (aresetn),
+    // inst sram interface
+    .inst_sram_req     (inst_sram_req    ),
+    .inst_sram_wr      (inst_sram_wr     ),
+    .inst_sram_size    (inst_sram_size   ),
+    .inst_sram_wstrb   (inst_sram_wstrb  ),
+    .inst_sram_addr    (inst_sram_addr   ),
+    .inst_sram_wdata   (inst_sram_wdata  ),
+    .inst_sram_addr_ok (inst_sram_addr_ok),
+    .inst_sram_data_ok (inst_sram_data_ok),
+    .inst_sram_rdata   (inst_sram_rdata  ),
+    // data sram interface
+    .data_sram_req     (data_sram_req    ),
+    .data_sram_wr      (data_sram_wr     ),
+    .data_sram_size    (data_sram_size   ),
+    .data_sram_wstrb   (data_sram_wstrb  ),
+    .data_sram_addr    (data_sram_addr   ),
+    .data_sram_wdata   (data_sram_wdata  ),
+    .data_sram_addr_ok (data_sram_addr_ok),
+    .data_sram_data_ok (data_sram_data_ok),
+    .data_sram_rdata   (data_sram_rdata  ),
+    // axi interface
+    // AR
+    .arid    (arid   ),
+    .araddr  (araddr ),
+    .arlen   (arlen  ),
+    .arsize  (arsize ),
+    .arburst (arburst),
+    .arlock  (arlock ),
+    .arcache (arcache),
+    .arprot  (arprot ),
+    .arvalid (arvalid),
+    .arready (arready),
+    // R
+    .rid    (rid   ),
+    .rdata  (rdata ),
+    .rresp  (rresp ),
+    .rlast  (rlast ),
+    .rvalid (rvalid),
+    .rready (rready),
+    // AW
+    .awid    (awid   ),
+    .awaddr  (awaddr ),
+    .awlen   (awlen  ),
+    .awsize  (awsize ),
+    .awburst (awburst),
+    .awlock  (awlock ),
+    .awcache (awcache),
+    .awprot  (awprot ),
+    .awvalid (awvalid),
+    .awready (awready),
+    // W
+    .wid    (wid   ),
+    .wdata  (wdata ),
+    .wstrb  (wstrb ),
+    .wlast  (wlast ),
+    .wvalid (wvalid),
+    .wready (wready),
+    // B
+    .bid    (bid   ),
+    .bresp  (bresp ),
+    .bvalid (bvalid),
+    .bready (bready)
+);
 endmodule
